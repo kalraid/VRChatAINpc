c0c1c94aa76afdccce2968510916dddd0c0f847f
kalraid <kimrie2@naver.com>
2020-04-06 14:03:40 +0900

    main project base setting

---
+++ VRChatAiNpcMain/src/main/java/com/vrchat/ai/npc/VrChatAiNpcMainApplication.java
@@ -0,0 +1,13 @@
+package com.vrchat.ai.npc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrChatAiNpcMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrChatAiNpcMainApplication.class, args);
+	}
+
+}
