a6edaa9476276b5259b2cce3aab136346d5fdacc
kalraid <kimrie2@naver.com>
2020-04-06 14:19:01 +0900

    body project base setting

---
+++ VRChatAiNpcBody/src/test/java/com/vrchat/ai/npc/VrChatAiNpcBodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrchat.ai.npc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrChatAiNpcBodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
