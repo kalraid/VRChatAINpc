8a5aa958dd8e08242357e4e673ff19a78d0eb917
kalraid <kimrie2@naver.com>
2020-04-09 18:38:30 +0900

    MAIN

---
+++ VRChatAiNpcBody/src/main/java/com/vrchat/ai/npc/VrChatAiNpcBodyApplication.java
@@ -0,0 +1,13 @@
+package com.vrchat.ai.npc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrChatAiNpcBodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrChatAiNpcBodyApplication.class, args);
+	}
+
+}
