fe9c4acb43ff9d9d528502a366d135d1742f87f0
kalraid <kimrie2@naver.com>
2020-04-23 16:07:36 +0900

    python mariadb 연동용 connector 완성

---
+++ VRChatAiNpcSpeaker/speaker/common/namespace/variable.py
@@ -0,0 +1,8 @@
+from enum import Enum
+class MYSQL(Enum):
+    HOST = "192.168.255.128"
+    PORT = "33305"
+    USER = "root"
+    PASSWORD = "g@KgUnHhcYUYu7."
+    DB_NAME = "data_speaker"
+    
--- VRChatAiNpcSpeaker/speaker/reinforcement/learning/learning.py
+++ VRChatAiNpcSpeaker/speaker/reinforcement/learning/learning.py
@@ -1,7 +1,13 @@
 from flask_restful import Resource
+from speaker.common.mariadb.connector import conn
+
 class learning(Resource):
     def get(self):
-        return 0
+        data = [];
+        connector = conn()
+        data.append(connector.getExecuteAll("select * from test", False))
+        data.append(connector.getExecuteAll("select * from test", True))
+        return data
     
     def post(self):
         return 1
