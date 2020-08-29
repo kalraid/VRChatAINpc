46d8caa596711cd227a274261f4f78aedc69038c
kalraid <kalraid@gmail.com>
2020-08-29 20:10:24 +0900

    test

---
+++ DnDPartes-1/src/test/java/com/dnd/party/search/DnDPartes1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.party.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnDPartes1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
