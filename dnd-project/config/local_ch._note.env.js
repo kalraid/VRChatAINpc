1bf0793b57a7cbb53acc426040b98dc102c2c375
kalraid <kimrie2@naver.com>
2020-09-01 22:56:12 +0900

    노트북 구동 확인

--- dnd-project/config/local_ch._note.env.js
+++ dnd-project/config/local_ch._note.env.js
@@ -5,5 +5,6 @@ const devEnv = require('./dev.env')
 module.exports = merge(devEnv, {
   NODE_ENV: '"ch_note"',
   BACK_URL : 'https://172.30.1.33:18080',
-  'process.env.HOST': '"172.30.1.33"'
+  'process.env.HOST': '"172.30.1.33"',
+  HOST: '172.30.1.33'
 })
