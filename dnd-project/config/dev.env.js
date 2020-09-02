2e9922e1eb37fdb229e84d0011366b10b4cd465e
kalraid <kimrie2@naver.com>
2020-09-02 17:41:59 +0900

    tt

--- dnd-project/config/dev.env.js
+++ dnd-project/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: ' "https://18.216.170.52:18080"'
+  API_URL: ' "http://18.216.170.52:18080"'
 })
